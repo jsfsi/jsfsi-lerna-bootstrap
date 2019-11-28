c508faff03a32c09701e2af0f36fe0ebfa18ad70
Joao Franco <joao.franco@focus-bc.com>
2019-11-28 21:57:30 +0000

    #CORE-26 Update to typescript 3.7

--- packages/rest-api-nodejs/src/index.ts
+++ packages/rest-api-nodejs/src/index.ts
@@ -2,4 +2,20 @@ import { RestServerBuilder, Logger } from '@jsfsi-core/typescript-nodejs'
 
 Logger.configure('debug')
 
+const a = {
+    b: 'test',
+    c: {
+        c1: 'test2',
+    },
+    d: {
+        d1: 'test3',
+    },
+}
+
+console.log(a?.b)
+console.log(a.c?.c1)
+a.d = null
+console.log(a.c ?? 'Nullish Coalescing')
+console.log(a.d ?? 'Nullish Coalescing')
+
 new RestServerBuilder().build().start()
